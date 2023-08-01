a114f7179e3b492bfc5fd7e9f7ccfa4b3126b89c
Chia <123429237+chiaxx@users.noreply.github.com>
2023-08-01 03:11:30 -0600

    Update 7. String Indexing.py

--- 7. String Indexing.py
+++ 7. String Indexing.py
@@ -23,3 +23,11 @@ credit_number = "1234-5678-9012-3456"
 credit_number = credit_number[::-1]
 print(credit_number)
 
+# MAIN EXERCISE
+# PYHTON EMAIL SLICER EXERCISE
+email = input("Enter your email: ")
+
+username = email[:email.index("@")]
+domain = email[email.index("@") + 1:]
+
+print(f"Your username is {username} and domain is {domain}")
